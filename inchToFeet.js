bee34bbb275a0f47e893324b3f86c4d8141702bb
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-01 19:20:57 +0600

    miles to km

---
+++ inchToFeet.js
@@ -0,0 +1,25 @@
+// const myInches = 12;
+// const myFeet = myInches /12;
+// console.log(myFeet);
+
+// const dadaInches = 144;
+// const dadaFeet = dadaInches /12;
+// console.log('dada feet', dadaFeet);
+
+// const dadiInches = 60;
+// const dadiFeet = dadiInches /12;
+// console.log('dadi feet', dadiFeet);
+
+
+function inchToFeet(inches){
+    const feet = inches /12;
+    return feet;
+}
+
+const dadaInches = 144;
+const dadaFeet = inchToFeet(dadaInches);
+console.log('dada feet',dadaFeet);
+
+const nanaInches = 168;
+const nanaFeet = inchToFeet(nanaInches);
+console.log('nana feet', nanaFeet);
